74e65c1e7a9b5273833227d3b9b2ff8058b3ba2a
DavidMC265 <93451599+DavidMC265@users.noreply.github.com>
2021-10-31 21:40:02 -0600

    Create inic.c

---
+++ inic.c
@@ -0,0 +1,13 @@
+#include "global.h"
+
+struct entrada.palsclave[] = {
+     "div", DIV,
+     "mod", MOD,
+    0,      0
+ };
+inic()        /* carga las palabras clave en la tabla de símbolos */
+{
+     struct entrada *p;
+     for (p = palscave; p ->complex; p++)
+          inserta(p->aplex, p->complex);
+}
