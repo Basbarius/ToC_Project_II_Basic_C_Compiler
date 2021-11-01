b5c15aeb6bbeb96c262da71fe83d3572b03bd8e6
DavidMC265 <93451599+DavidMC265@users.noreply.github.com>
2021-10-31 21:49:42 -0600

    Create error.c

---
+++ error.c
@@ -0,0 +1,10 @@
+/*****     error.c      ********************/
+
+#include "global.h"
+
+error(m)      /* genera todos los mensajes de error */
+      char *m;
+{
+      fprintf(stderr, "línea %d: %s/n", numlínea, m);
+      exit(1);    /* terminación sin éxito */ 
+}
