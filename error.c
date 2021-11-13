568dad3a85fe9df3b0e8625bbe40f36cf596eff0
Bas Bargagli <bas.bargagli@gmail.com>
2021-11-12 18:23:27 -0600

    correccion print error

--- error.c
+++ error.c
@@ -5,6 +5,6 @@
 error(m)      /* genera todos los mensajes de error */
       char *m;
 {
-      fprintf(stderr, "linea %d: %s/n", numlinea, m);
+      fprintf(stderr, "linea %d: %s\n", numlinea, m);
       exit(1);    /* terminación sin éxito */ 
 }
